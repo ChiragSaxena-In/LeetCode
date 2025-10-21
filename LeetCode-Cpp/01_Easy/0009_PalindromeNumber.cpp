e0b6b9faa9a5a1ab8747d21996543c8cbf347209
Chirag Saxena <saxenachirag349@gmail.com>
2025-10-21 13:44:21 +0530

    Create 0009_PalindromeNumber.cpp

---
+++ LeetCode-Cpp/01_Easy/0009_PalindromeNumber.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        // Negative numbers are not palindromes
+        // Numbers ending with 0 (but not 0 itself) can't be palindrome
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        long long int rev_num = 0;
+        for(int i = x; i > 0; i /= 10){
+            rev_num = rev_num*10+i%10;
+        }
+        if (rev_num == x) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
